1f50d8bbd7ab228742d211b6a6cea734d216a083
Chetsadakon <chetsadakon@codediva.co.th>
2023-02-01 16:13:14 +0700

    sort add variable

--- chap5/sort.js
+++ chap5/sort.js
@@ -4,8 +4,8 @@ var f2 = ['Banana', 'Orange', 'Apple', 'Mango']
 console.log(f2.reverse())
 var a1 = ['หมู', 'หมา', 'กา' , 'ไก่']
 a1.sort()
-var a1 = ['หมู', 'หมา', 'กา' , 'ไก่']
-a1.sort()
+var a2 = a1.sort()
+console.log(a2)
 var a1 = ['หมู', 'หมา', 'กา' , 'ไก่']
 a1.sort()
 var a1 = ['หมู', 'หมา', 'กา' , 'ไก่']
